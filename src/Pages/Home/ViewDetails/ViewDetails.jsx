b76f6f2a8c25ce8b0f6740d39e4c7b7d1c68cb13
rahatul <rahatul99@gmail.com>
2023-05-21 23:15:27 +0600

    update addtoy section

--- src/Pages/Home/ViewDetails/ViewDetails.jsx
+++ src/Pages/Home/ViewDetails/ViewDetails.jsx
@@ -1,21 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 
 const ViewDetails = () => {
-  const {name, price, rating, picture,description, available_quantity,seller} = useLoaderData();
+  const { name, price, rating, picture, description, available_quantity, seller } = useLoaderData();
   return (
-    <div>
-        <div>
-          <h2>{name}</h2>
-          <h1>{description}</h1>
-          <h1>{available_quantity}</h1>
-          <h1>{seller}</h1>
-          <p>Price: {price}</p>
-          <p>Rating: {rating}</p>
-          <img src={picture} alt={name} />
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="max-w-md bg-white rounded-lg overflow-hidden shadow-lg">
+        <img className="w-full h-48 object-cover" src={picture} alt={name} />
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-2">{name}</h2>
+          <p className="text-gray-700 mb-2">{description}</p>
+          <p className="text-gray-700 mb-2">Available Quantity: {available_quantity}</p>
+          <p className="text-gray-700 mb-2">Seller: {seller}</p>
+          <p className="text-lg font-bold mb-2">Price: {price}</p>
+          <p className="text-lg font-bold mb-2">Rating: {rating}</p>
         </div>
-      <h1>hello</h1>
+      </div>
     </div>
   );
 };
 
 export default ViewDetails;
+
